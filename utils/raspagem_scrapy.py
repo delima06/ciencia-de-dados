027b05164aabbf67b8b805d447febcb28c5a51b9
Thiago de Lima <thiagochec3@gmail.com>
2026-08-25 20:09:06 -0300

    Add custom settings to SpiderWikipedia and CrawlerRunner

--- utils/raspagem_scrapy.py
+++ utils/raspagem_scrapy.py
@@ -1,11 +1,17 @@
-%%writefile utils/raspagem_scrapy.py
 import scrapy
 from scrapy.crawler import CrawlerRunner
+from scrapy.settings import Settings
 from crochet import wait_for
 import time
 
 class SpiderWikipedia(scrapy.Spider):
     name = "wiki_spider"
+    
+    custom_settings = {
+        'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+        'LOG_LEVEL': 'ERROR',
+        'TWISTED_REACTOR': 'twisted.internet.epollreactor.EPollReactor'
+    }
     
     def __init__(self, start_urls, lista_textos, *args, **kwargs):
         super(SpiderWikipedia, self).__init__(*args, **kwargs)
@@ -19,7 +25,10 @@ class SpiderWikipedia(scrapy.Spider):
 
 @wait_for(timeout=60.0)
 def rodar_spider(urls, lista_textos):
-    runner = CrawlerRunner()
+    config = Settings()
+    config.set('TWISTED_REACTOR', 'twisted.internet.epollreactor.EPollReactor')
+    config.set('LOG_LEVEL', 'ERROR')
+    runner = CrawlerRunner(config)
     return runner.crawl(SpiderWikipedia, start_urls=urls, lista_textos=lista_textos)
 
 def raspar_com_scrapy(lista_urls):
